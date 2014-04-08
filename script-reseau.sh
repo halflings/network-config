1acb4033076ff6a49db467464d21ea397cc26082
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-08 09:59:16 +0200

    Updates script-reseau.sh

--- script-reseau.sh
+++ script-reseau.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-vconfig add p17p1 2
-vconfig add p17p1 3
-vconfig add p17p1 4
-vconfig add p17p1 5
-vconfig add p17p1 6
+sudo ifconfig p17p1 up
 
-ifconfig p17p1.2 up
-ifconfig p17p1.3 up
-ifconfig p17p1.4 up
-ifconfig p17p1.5 up
-ifconfig p17p1.6 up
+sudo vconfig add p17p1 2
+sudo vconfig add p17p1 3
+sudo vconfig add p17p1 4
+sudo vconfig add p17p1 5
+sudo vconfig add p17p1 6
+
+sudo ifconfig p17p1.2 up
+sudo ifconfig p17p1.3 up
+sudo ifconfig p17p1.4 up
+sudo ifconfig p17p1.5 up
+sudo ifconfig p17p1.6 up
