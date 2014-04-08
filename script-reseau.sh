6aa98537881b3d01c073c0ffd1bc65ae7114c511
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-04-08 09:54:30 +0200

    Implements script-reseau.sh

---
+++ script-reseau.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+vconfig add p17p1 2
+vconfig add p17p1 3
+vconfig add p17p1 4
+vconfig add p17p1 5
+vconfig add p17p1 6
+
+ifconfig p17p1.2 up
+ifconfig p17p1.3 up
+ifconfig p17p1.4 up
+ifconfig p17p1.5 up
+ifconfig p17p1.6 up
